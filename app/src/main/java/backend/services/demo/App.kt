fda6d9a263a66b5a7d18796fa3c10a9f0c4dfbea
doorbash <milad.doorbash@gmail.com>
2021-09-13 16:46:09 +0430

    add App

---
+++ app/src/main/java/backend/services/demo/App.kt
@@ -0,0 +1,27 @@
+package backend.services.demo
+
+import android.R
+import android.app.Application
+import backend.services.Client
+import backend.services.ClientOptions
+import backend.services.notifications.BackendServicesNotificationsClient
+import backend.services.rc.BackendServicesRemoteConfigClient
+
+class App : Application() {
+    override fun onCreate() {
+        Client.init(
+            this, ClientOptions(
+                BuildConfig.VERSION_CODE,
+                "com.example.project",
+                "https://doorbash.ir/api",
+                10,
+                false,
+                R.drawable.btn_star,
+                MainActivity::class.java
+            )
+        )
+        BackendServicesNotificationsClient.enqueueWorker(this)
+        BackendServicesRemoteConfigClient.enqueueWorker(this)
+        super.onCreate()
+    }
+}
